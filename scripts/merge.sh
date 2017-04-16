18fff432ca786b16b8c10b8bf04afcdc885638a5
@theshapguy <me@theshapguy.com>
2017-04-16 03:14:56 -0600

    fix: updated commit message
    [skip ci]

--- scripts/merge.sh
+++ scripts/merge.sh
@@ -8,7 +8,7 @@ fi
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
       hugo -d public
-      echo "Travis should not deploy from pull requests"
+      echo "travis should not deploy from pull requests"
       exit 0
 fi
 
@@ -21,8 +21,8 @@ rm -rf /tmp/public
 mv public /tmp
 mv scripts /tmp/public/scripts
 
-git config user.name "Travis CI"
-git config user.email "ci@travis.com"
+git config user.name "auto: Shap Guy"
+git config user.email "me@theshapguy.com"
 git config --global push.default simple
 
 git branch -a
@@ -43,7 +43,7 @@ git add -A
 export TZ=":America/Denver"
 now=$(date +"%Y-%m-%d %H:%M")
 
-msg=":closed_book: Travis: rebuilding site $now"
+msg=":bookmark: travis: rebuilding site $now"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
